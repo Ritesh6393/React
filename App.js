d73b7ab26817a10ec3981b1135aea6223a104fb4
Ritesh Rai <riteshrai9236@gmail.com>
2024-08-24 00:59:05 +0530

    create react app without jsx

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent=React.createElement("div",
+    {id:"parent"},
+    [React.createElement("div",
+      {id:"child1"},
+      [React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"i am h2")]  
+    ),
+    React.createElement("div",
+        {id:"child2"},
+        [React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"i am h2")]  
+    )
+]
+)
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
